c10e223e28de5e20250c197d628b373c18d5cbbe
karthikpk-o <kpunichathayak@gmail.com>
2023-12-07 12:36:01 +0530

    solved expenditure-analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,33 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory(transactions) 
+{
+	let lst = [];
+	for(let i=0; i<transactions.length; i++)
+	{
+		let cat_exists = false;
+		let cat = transactions[i]["category"]
+		for(let j=0; j<lst.length; j++)
+		{
+			if(lst[j]["category"] === cat)
+			{
+				cat_exists = true;
+				lst[j]["totalSpent"] += transactions[i]["price"];
+				break;
+			}
+		}
+		if(!cat_exists)
+		{
+			let price = transactions[i]["price"];
+			ele = {
+			category: cat,
+			totalSpent: price,
+			}
+			lst.push(ele)
+		}
+	}
+  	return lst;
 }
 
 module.exports = calculateTotalSpentByCategory;
