b90de03008b18c03deabaf0e9e0bfc36b20c25a1
karthikpk-o <kpunichathayak@gmail.com>
2023-12-07 17:13:59 +0530

    solved medium-palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,11 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
-  return true;
+function isPalindrome(str) 
+{
+  let cleaned_str = str.replace(" ",'').replace(/[^\w]/g ,'').toLowerCase();
+  let str_rev = cleaned_str.split('').reverse().join('');
+  return str_rev == cleaned_str
 }
 
 module.exports = isPalindrome;
